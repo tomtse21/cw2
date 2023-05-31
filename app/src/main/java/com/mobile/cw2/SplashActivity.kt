4b5da4af50104a2429826cb524276ed2bd20ed16
hohopk2001 <takeaerst21@gmail.com>
2023-05-31 18:40:27 +0800

    auth

---
+++ app/src/main/java/com/mobile/cw2/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.mobile.cw2
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mobile.cw2.databinding.ActivityMainBinding
+
+class SplashActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun checkDeviceHasBiometric(){
+        val biometricManager = BiometricManager.from(this)
+    }
+//    private fun proceedToHomeScreen() {
+//        Log.d("123","asd")
+//        // Delay the transition to simulate a splash screen
+//        Handler().postDelayed({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, SPLASH_DELAY.toLong())
+//    }
+
+
+
+}
---
+++ app/src/main/java/com/mobile/cw2/TextReg.kt
@@ -0,0 +1,240 @@
+package com.mobile.cw2
+
+import android.Manifest
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Menu
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.lang.Exception
+
+class TextReg: AppCompatActivity() {
+    private lateinit var inputImageBtn: MaterialButton
+    private lateinit var recognitionBtn: MaterialButton
+    private lateinit var imageIv: ImageView
+    private lateinit var recognizedTextEt: EditText
+
+    private companion object{
+        private const val CAMERA_REQUEST_CODE = 100
+        private const val STORAGE_REQUEST_CODE = 101
+    }
+
+    private var imageUri: Uri? = null
+    private lateinit var cameraPermissions: Array<String>
+    private lateinit var storageaPermissions: Array<String>
+
+    private lateinit var progressDialog: ProgressDialog
+
+    private lateinit var textRecognizer: TextRecognizer
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.text_reg)
+
+
+        inputImageBtn = findViewById(R.id.inputImageBtn)
+        recognitionBtn = findViewById(R.id.recognizeTextBtn)
+        imageIv = findViewById(R.id.imageIv)
+        recognizedTextEt = findViewById(R.id.recognizedTextEt)
+
+        cameraPermissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        storageaPermissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        inputImageBtn.setOnClickListener{
+            showInputImageDialog()
+        }
+
+        recognitionBtn.setOnClickListener{
+            if(imageUri == null){
+                showToast("Pick Image Frist,.,,")
+            }else{
+                recognizeTextFromImage()
+            }
+        }
+    }
+
+
+    private fun recognizeTextFromImage() {
+        progressDialog.setMessage("Preparing image")
+        progressDialog.show()
+
+        try {
+            val inputImage = InputImage.fromFilePath(this, imageUri!!)
+            progressDialog.setMessage("Recognizing text...")
+
+            val textTaskResult = textRecognizer.process(inputImage)
+                .addOnSuccessListener { text->
+                    progressDialog.dismiss()
+                    val recognizedText = text.text
+
+                    recognizedTextEt.setText(recognizedText)
+                }
+                .addOnFailureListener{e->
+                    progressDialog.dismiss()
+                    showToast("Failed to prepare image due to ${e.message}")
+                }
+
+        }catch (e: Exception){
+            progressDialog.dismiss()
+            showToast("Failed to prepare image due to ${e.message}")
+        }
+    }
+
+    private fun showInputImageDialog() {
+        val popMenu = PopupMenu(this,inputImageBtn)
+        popMenu.menu.add(Menu.NONE, 1, 1, "CAMERA")
+        popMenu.menu.add(Menu.NONE, 2, 2, "GALLERY")
+
+        popMenu.show()
+
+        popMenu.setOnMenuItemClickListener { menuItem ->
+            val id = menuItem.itemId
+
+            if(id == 1){
+                if(checkCameraPermission()){
+                    pickImageCamera()
+                }else{
+                    pickImageGallery()
+//                    requestCameraPermission()
+                }
+            }else if(id == 2){
+                if(checkStoragePermission()){
+                    pickImageGallery()
+                }else{
+                    requestStoragePermission()
+                }
+            }
+            return@setOnMenuItemClickListener  true
+
+        }
+    }
+
+    private fun pickImageGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type="image/*"
+        galleryActivityResultLauncher.launch(intent)
+
+    }
+
+    private val  galleryActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+
+            if(result.resultCode == Activity.RESULT_OK){
+                val data = result.data
+                imageUri = data!!.data
+                imageIv.setImageURI(imageUri)
+            }else{
+                showToast("Canceled")
+            }
+
+
+        }
+
+    private fun pickImageCamera(){
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Sample  Title");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Sample Title");
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraActivityResultLauncher.launch(intent)
+    }
+
+    private val cameraActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+
+            if(result.resultCode == Activity.RESULT_OK){
+                imageIv.setImageURI(imageUri)
+            }else{
+                showToast("Canceled")
+            }
+
+
+        }
+    private fun checkStoragePermission(): Boolean{
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkCameraPermission(): Boolean{
+        val cameraResult = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        val storageResult = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+        return cameraResult && storageResult
+    }
+
+    private fun requestStoragePermission(){
+        ActivityCompat.requestPermissions(this,storageaPermissions,
+            STORAGE_REQUEST_CODE
+        )
+    }
+
+    private fun requestCameraPermission(){
+        ActivityCompat.requestPermissions(this,storageaPermissions,
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+
+            CAMERA_REQUEST_CODE ->{
+                if(grantResults.isNotEmpty()){
+                    val cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    val storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED
+
+                    if(cameraAccepted && storageAccepted){
+                        pickImageCamera()
+                    }else{
+                        showToast("Camera & Storage permission are required...")
+                    }
+                }
+            }
+
+            STORAGE_REQUEST_CODE ->{
+                if(grantResults.isNotEmpty()){
+                    val storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    if(storageAccepted){
+                        pickImageGallery()
+                    }else{
+                        showToast("Storage permission are required...")
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+}
