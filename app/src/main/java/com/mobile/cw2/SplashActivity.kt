9a9634641de752b11159972dbf76566d7ebcd268
hohopk2001 <takeaerst21@gmail.com>
2023-06-07 23:24:33 +0800

    added faceid and fingerprint

--- app/src/main/java/com/mobile/cw2/SplashActivity.kt
+++ app/src/main/java/com/mobile/cw2/SplashActivity.kt
@@ -1,21 +1,89 @@
 package com.mobile.cw2
 
 import android.content.Intent
-import android.hardware.biometrics.BiometricManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.PersistableBundle
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.mobile.cw2.AccessPermission
+import com.mobile.cw2.databinding.ActivitySplashBinding
+import java.util.concurrent.Executor
 
 class SplashActivity: AppCompatActivity() {
 
+    lateinit var binding: ActivitySplashBinding
+//    lateinit var info: String
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // requestPermission()
-        proceedToHomeScreen()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.isEnabled = true
+        binding.imgFigure.setOnClickListener {
+            checkDeviceHasBiometric()
+        }
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence,
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult,
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                    proceedToHomeScreen()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+
+        binding.button.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+
+
     }
 
     private fun proceedToHomeScreen() {
@@ -38,5 +106,39 @@ class SplashActivity: AppCompatActivity() {
         }, 1000)
     }
 
+    private fun checkDeviceHasBiometric() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.")
+//                info = "App can authenticate using biometrics."
+                binding.button.isEnabled = true
+
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                Log.e("MY_APP_TAG", "No biometric features available on this device.")
+//                info = "No biometric features available on this device."
+                binding.button.isEnabled = false
+
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
+//                info = "Biometric features are currently unavailable."
+                binding.button.isEnabled = false
+
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                // Prompts the user to create credentials that your app accepts.
+                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                    putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+                }
+                binding.button.isEnabled = false
+
+                startActivityForResult(enrollIntent, 100)
+            }
+        }
+//        binding.tvShowMsg.text = info
+    }
 
 }
