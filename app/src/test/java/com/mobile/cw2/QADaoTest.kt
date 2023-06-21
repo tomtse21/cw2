3d02eacb31e267db1acd0185355fa3875a50f75e
tomtse21 <227021367@stu.vtc.edu.hk>
2023-06-21 21:13:32 +0800

    doing unit test

---
+++ app/src/test/java/com/mobile/cw2/QADaoTest.kt
@@ -0,0 +1,25 @@
+package com.mobile.cw2
+
+import android.app.Application
+import androidx.room.Room
+import com.mobile.cw2.uitl.AppDatabase
+import com.mobile.cw2.uitl.QA
+import com.mobile.cw2.uitl.QADao
+import org.junit.Before
+
+class QADaoTest {
+
+    lateinit var qaDB: AppDatabase
+    lateinit var qaDao: QADao
+
+
+    @Before
+    fun setUp(){
+
+    }
+
+    @Before
+    fun tearDown(){
+
+    }
+}
